14051dc4ae93b18a6641d23a17fcfa8a7a311f6e
Anrodmo <131297046+Anrodmo@users.noreply.github.com>
2023-11-06 21:25:58 +0100

    Añadidos en Main
    Añadido souts para facilitar comprensión del método Main

--- src/ad_proyecto1/AD_Proyecto1.java
+++ src/ad_proyecto1/AD_Proyecto1.java
@@ -20,20 +20,26 @@ public class AD_Proyecto1 {
     public static void main(String[] args) {
         String ruta = "./src/ad_proyecto1/Libros.xml";
         String ruta2 = "./src/ad_proyecto1/Libros2.xml";
-
         AccesoDOM acceso = new AccesoDOM();
         
         if(ruta==null)ruta=""; // Evito la PointNullException al crear File
         File archivo = new File(ruta);
         
+        
+        System.out.println("\n ================Abrimos XML y mostramos===========================\n");
         acceso.abrirXMLaDom(archivo);              
         acceso.recorreDOMyMuestra();
         
+        System.out.println("\n================Creamos libro y mostramos --> El fari  ================= \n");
         acceso.insertarLibroEnDOM("El fari", "las aventuras", "01", "aventuras",
                 "50", "01/01/1990", "las aventuras de un taxista");
         acceso.recorreDOMyMuestra();
+        System.out.println("\n ================Borramos las aventuras y mostramos======================\n");
+        
         acceso.deleteNode("las aventuras");
         acceso.recorreDOMyMuestra();
+        
+        System.out.println("\n ================ Guardamos en un nuevo archivo.======================\n");
         acceso.guardarDomcomoArchivo(ruta2);
         
         
